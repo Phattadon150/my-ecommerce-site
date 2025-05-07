b1032cac8f340a4807ade3078c81872a8bba8fa0
Phattadon150 <phattadon_d@cmu.ac.th>
2025-05-07 12:00:07 +0700

    fiexed

---
+++ expServer.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require ('path');
+
+const app = express ();
+
+//serve static files form current project directory 
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname,"index.html"));
+});
+
+app.listen(3000, () => {
+    console.log("Sever running at http://localhost:3000")
+})
